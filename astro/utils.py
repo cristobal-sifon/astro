5e9af48a663b409948f0ce0407179ca4247bdbfc
Cristóbal Sifón <cristobal.sifon@pucv.cl>
2022-10-11 12:09:11 -0300

    initial working makereg

--- astro/utils.py
+++ astro/utils.py
@@ -5,77 +5,61 @@ Generic astronomical utilities
 from __future__ import absolute_import, division, print_function
 
 #import anydbm
-import os
-import re
-import readfile
-import scipy
-import string
-import urllib
 from astLib.astWCS import WCS
+import warnings
 
 
-def makereg(filename, rakey='ra', deckey='dec', xkey='x', ykey='y',
-            shape='circle', size='3"', color='magenta',
-            commentkeys='', wcs=None, frame='default', output=None):
+def makereg(x, y, text=None, frame='fk5',
+            shape='circle', size='3"',
+            properties={'color': 'yellow'},
+            commentkeys='', output='ds9.reg'):
     """
     Take a galaxy catalog and create a region file. See
     http://ds9.si.edu/doc/ref/region.html.
 
+    **Only circles for now**
+
     Parameters
     ----------
-        filename    : str
-            input filename, containing at least two positional columns,
-            (x,y) or (ra,dec)
-        rakey       : str, int or None
-            Right ascension column name (if header is provided in the
-            first row) or column number. Will skip if name not present
-            in header.
-        deckey      : str, int or None
-            Declination column name (if header is provided in the first
-            row) or column number. Will skip if name not present in
-            header.
-        xkey, ykey  : str or int
-            analogous to rakey, deckey for image coordinates. If rakey
-            and deckey are valid entries then xkey and ykey are
-            ignored.
-        shape       : str
-            any shape accepted by DS9 region files.
-        size        : str
-            comma-separated size(s) and rotation angle, as appropirate
-            for the selected shape. For instance, for `shape=circle`,
-            `size` could be '3"' (three arcsec) or '4' (four pixels);
-            and for `shape=box`, `size` could be '3",4",45'
-            (three-by-four sq. arcsec rotated by 45 degrees).
-        color       : str
-            any color accepted by DS9 region files.
-        commentkeys : str, int, or lists of them (optional)
-                      analogous to the above for any text to be added
-                      to the region file on top of the objects.
-        wcs         : str or astLib.astWCS.WCS object (optional)
-            image with which to convert coordinates
-        frame       : {'fk5', 'image'} (optional)
-            frame in which the region file will be written (WCS
-            or image coordinates). If not specified, will use the same
-            input frame.
-        output      : str (optional)
-            output file name. If not provided, will simply replace
-            the extension of the original file by .reg.
+    x, y : array-like
+        coordinates in appropriate system
+
+    Optional Parameters
+    -------------------
+    text : array-like
+        text to add next to each region
+    frame : one of ('fk5', 'image')
+        frame in which the region file will be written.
+    shape : str
+        any shape accepted by DS9 region files.
+    size : str
+        comma-separated size(s) and rotation angle, as appropirate
+        for the selected shape. For instance, for `shape=circle`,
+        `size` could be '3"' (three arcsec) or '4' (four pixels);
+        and for `shape=box`, `size` could be '3",4",45'
+        (three-by-four sq. arcsec rotated by 45 degrees).
+    color : str
+        any color accepted by DS9 region files.
+    output : str
+        output file name.
 
     """
-    hdr = open(filename).readline().split()
-    # are the keys given as column names?
-    if (isinstance(rakey, basestring) \
-            and (rakey in hdr and deckey in hdr)) \
-            or (isinstance(xkey, basestring) \
-                and (xkey in hdr and ykey in hdr)):
-        if rakey in hdr and deckey in hdr:
-            keys = ','.join((rakey, deckey))
-        else:
-            keys = ','.join((xkey, ykey))
-        if hasattr(commentkeys, '__iter__'):
-            commentkeys = ','.join(commentkeys)
-        if isinstance(commentkeys, basestring) and len(commentkeys) > 0:
-            keys = ','.join((keys, commentkeys))
-
+    if shape != 'circle':
+        wrn = 'only circle implemented for now'
+        warnings.warn(wrn)
+        shape = 'circle'
+    if text is None:
+        X = (x, y)
+    else:
+        X = (x, y, text)
+    with open(output, 'w') as reg:
+        print(frame, file=reg)
+        props = ' '.join([f'{key}={val}' for key, val in properties.items()])
+        print(f'global {props}', file=reg)
+        for i in zip(*X):
+            xi, yi = i[:2]
+            msg = f'{shape}({xi},{yi},{size})'
+            if text is not None:
+                msg = f'{msg} # text={i[2]}'
+            print(msg, file=reg)
     return
-
